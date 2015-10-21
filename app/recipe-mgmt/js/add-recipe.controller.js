7f1e2583ea69d4ad0b6d9cbae655250cd1ec022b
Yasa Yener <yyener@uni-koeln.de>
2015-10-21 12:54:12 +0200

    Added basic controller, template and localization for add-recipe dialog

---
+++ app/recipe-mgmt/js/add-recipe.controller.js
@@ -0,0 +1,5 @@
+angular.module('app.recipe-mgmt')
+    .controller('AddRecipeCntl', function ($scope) {
+        'use strict';
+
+    });
--- app/recipe-mgmt/js/recipe-mgmt.module.js
+++ app/recipe-mgmt/js/recipe-mgmt.module.js
@@ -1,3 +1,18 @@
 /**
  * Created by TH-Koeln on 20.10.2015.
  */
+angular.module('app.recipe-mgmt', ['app.main'])
+    .config(function ($routeProvider, oaspTranslationProvider) {
+        'use strict';
+		oaspTranslationProvider.enableTranslationForModule('recipe-mgmt');
+        $routeProvider.when('/recipe-mgmt/recipe-add', {
+            templateUrl: 'recipe-mgmt/html/recipe-add.html',
+            controller: 'AddRecipeCntl',
+            resolve: {
+                currentPositions: ['positions', function (positions) {
+                    return positions.get();
+                }]
+            }
+        });
+    });
+
