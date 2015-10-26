0c5c387f80478b74ec10b848d6bef202385a78ab
Marc Schwede <schwedenmut@googlemail.com>
2015-10-26 16:22:01 +0100

    Login Screen now has random 3 menus displayed.

--- app/main/js/sign-in.controller.js
+++ app/main/js/sign-in.controller.js
@@ -14,5 +14,35 @@ angular.module('app.main')
             {bild: "http://files.schwedenmut.de/dorade.jpg", bezeichnung: "Gebackene Dorade mit scharfen Ofenkartoffeln", beschreibung: "Test 4", preis: "17,00"},
             {bild: "http://files.schwedenmut.de/sushi.jpg", bezeichnung: "Süßes Sushi", beschreibung: "Eine süße Kombination aus Milchreis, Kokos, Mangos, Erdbeeren und Kiwis präsentiert als Sushi.", preis: "8,90"}
         ];
-        
+        //How many Items to display
+        $scope.quantity = 3;
+        // Fisher–Yates shuffle algorithm
+        $scope.shuffleArray = function(array) {
+            var m = array.length, t, i;
+            // While there remain elements to shuffle
+            while (m) {
+                // Pick a remaining element…
+                i = Math.floor(Math.random() * m--);
+                // And swap it with the current element.
+                t = array[m];
+                array[m] = array[i];
+                array[i] = t;
+            }
+            return array;
+        }
+        $scope.shuffledGerichteListe = $scope.shuffleArray($scope.gerichteListe);
+
+
     });
+
+angular.module('app.main').directive('menu', function(){
+        return function (scope, element, attrs) {
+            element.height($(window).height() - $('.navbar-fixed-top').outerHeight() - '75');
+        }
+    });
+//Change the height of menuItems to fit the screen
+angular.module('app.main').directive('menuitem', function(){
+    return function (scope, element, attrs) {
+        element.height(($(window).height() - $('.navbar-fixed-top').outerHeight() - '75')/ scope.quantity);
+    }
+});
