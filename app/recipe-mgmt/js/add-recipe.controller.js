8bedfe6cff98a62c3da32a416f665497528b7be5
Yasa Yener <yyener@uni-koeln.de>
2015-10-27 15:21:32 +0100

    File dialog will now convert a provided image to base64 and add it to the serialized recipe JSON

--- app/recipe-mgmt/js/add-recipe.controller.js
+++ app/recipe-mgmt/js/add-recipe.controller.js
@@ -5,7 +5,25 @@ angular.module('app.recipe-mgmt')
         $scope.recipeName = '';
         $scope.recipeDescription = '';
         $scope.recipePrice = '';
-        //$scope.recipeImage = '';
+        $scope.recipeImage = null;
+
+        $scope.imageBusy = false;
+
+        document.getElementById('recipeImage').addEventListener('change', function(event) {
+        	var file = event.target.files[0];
+        	var reader = new FileReader();
+        	reader.onload = function(e) {
+        		$scope.recipeImage = e.target.result;
+        	}
+        	reader.onloadstart = function(e) {
+        		$scope.imageBusy = true;
+        	}
+        	reader.onloadend = function(e) {
+        		$scope.imageBusy = false;
+        	}
+        	// Convert image to BASE64
+        	reader.readAsDataURL(file);
+        }, false);
 
         $scope.findRecipes = function() {
         	console.log(recipes.loadRecipe(0));
@@ -16,7 +34,7 @@ angular.module('app.recipe-mgmt')
         					"name": $scope.recipeName,
         					"description": $scope.recipeDescription,
         					"price": $scope.recipePrice,
-        					"image": null //$scope.recipeImage
+        					"image": $scope.recipeImage
         				};
         	recipes.saveRecipe(recipe);
         }
