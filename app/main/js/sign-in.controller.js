5e296de115ef0cbb2f4225301e8d25b1ae5e9c15
Marc Schwede <schwedenmut@googlemail.com>
2015-10-28 13:06:46 +0100

    In order to comply with the Code Conventions, directives were stored in a separate file.

--- app/main/js/sign-in.controller.js
+++ app/main/js/sign-in.controller.js
@@ -34,17 +34,3 @@ angular.module('app.main')
 
 
     });
-
-angular.module('app.main').directive('menu', function($window){
-        'use strict';
-        return function (scope, element) {
-            element.height($($window).height() - $('.navbar-fixed-top').outerHeight() - '75');
-        };
-    });
-//Change the height of menuItems to fit the screen
-angular.module('app.main').directive('menuitem', function($window){
-    'use strict';
-    return function (scope, element) {
-        element.height(($($window).height() - $('.navbar-fixed-top').outerHeight() - '75')/ scope.quantity);
-    };
-});
