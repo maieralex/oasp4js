b9fff4ddf78a4d8f04db437cf1209608fce91934
Marc Schwede <schwedenmut@googlemail.com>
2015-11-02 15:03:29 +0100

    added testfile for the gerichte-liste rest service (called from the login page).

---
+++ app/main/js/gerichte-liste.rest.service.spec.js
@@ -0,0 +1,22 @@
+/**
+ * Created by mschwede on 02.11.15.
+ */
+describe('Module: app.main, Service: gerichteListeRestService', function () {
+    'use strict';
+    var gerichteListeRestService, contextPath = '/contextPath/';
+
+    beforeEach(module('app.main', function ($provide) {
+        $provide.value('currentContextPath', oasp.mock.currentContextPathReturning(contextPath));
+    }));
+
+    beforeEach(inject(function (_gerichteListeRestService_) {
+        gerichteListeRestService = _gerichteListeRestService_;
+        }));
+
+    it('should call $http.get when gerichteListeRestService.getRecipe is called', inject(function ($http) {
+        var params = {id: '0'};
+        spyOn($http, 'get');
+        gerichteListeRestService.getRecipe(params);
+        expect($http.get).toHaveBeenCalledWith(contextPath + 'services/rest/recipemanagement/v1/recipe/'+params);
+    }));
+});
