fb43b33c6a32d9d7b097ffe4276dd0c01c59cf3f
Max Kutscher <max.kutscher@smail.th-koeln.de>
2015-11-03 16:16:07 +0100

    update of the tests for the recipe-add.controller

--- app/recipe-mgmt/js/recipe-add.controller.spec.js
+++ app/recipe-mgmt/js/recipe-add.controller.spec.js
@@ -3,10 +3,17 @@ describe('Modul:recipe-mgmt, Controller: add recipe',function() {
     beforeEach(module('app.recipe-mgmt'));
 
     var addRecipeController,
+        /*
+        mockRecipes = {
+            saveRecipes: function(){
+                return 'OK';
+            }
+        },*/
         scope;
 
     beforeEach(inject(function($rootScope, $controller){
-        scope = $rootScope.$new();
+            scope = $rootScope.$new();
+            //recipes =  mockRecipes;
             addRecipeController = $controller('RecipeAddCntl',{
                 $scope: scope
             });
@@ -17,6 +24,7 @@ describe('Modul:recipe-mgmt, Controller: add recipe',function() {
         expect(scope.recipeName).not.toBeNull();
         expect(scope.recipeName).toBe('');
     });
+
     it('should check the init recipe description', function() {
         expect(scope.recipeDescription).not.toBeNull();
         expect(scope.recipeDescription).toBe('');
@@ -36,19 +44,30 @@ describe('Modul:recipe-mgmt, Controller: add recipe',function() {
     });
     it('should check the set recipe description', function() {
         scope.recipeDescription = 'MyRecipe description';
-       expect(scope.recipeDescription).not.toBeNull();
-       expect(scope.recipeDescription).toBe('MyRecipe description');
+        expect(scope.recipeDescription).not.toBeNull();
+        expect(scope.recipeDescription).toBe('MyRecipe description');
     });
     it('should check the set recipe price', function() {
         scope.recipePrice = '6.5';
-       expect(scope.recipePrice).not.toBeNull();
-       expect(scope.recipePrice).toBe('6.5');
+        expect(scope.recipePrice).not.toBeNull();
+        expect(scope.recipePrice).toBe('6.5');
     });
 
+
     /*
-    it('should check the set recipe image', function() {
-       expect(scope.recipeImage).not.toBeNull();
-       expect(scope.recipeImage).toBe('');
+     it('should check the set recipe image', function() {
+     expect(scope.recipeImage).not.toBeNull();
+     expect(scope.recipeImage).toBe('');
+     });
+     */
+
+    it('should check the existence and execution of adding a recipe functionality', function() {
+        expect(scope.saveRecipe).not.toBe(null);
+        expect(scope.saveRecipe).toBeDefined();
+
+        spyOn(scope,'saveRecipe');
+        scope.saveRecipe(null);
+        expect(scope.saveRecipe).toHaveBeenCalled();
     });
-    */
+
 });
