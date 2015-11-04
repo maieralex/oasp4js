2d380667bbe4bb81ef4006ccf111a5a69c742fcb
Dennis Nahlik <dnahlik@p058166.nat-gm.FH-Koeln.DE>
2015-11-04 16:40:50 +0100

    Tests for rest-service.spec and service.spec

---
+++ app/recipe-mgmt/js/recipe-mgmt.rest.service.spec.js
@@ -0,0 +1,42 @@
+/*globals oasp*/
+describe('Module: recipeMgmt, Service: recipeManagementRestService', function () {
+    'use strict';
+    var recipeManagementRestService, contextPath = '/contextPath/';
+
+    beforeEach(module('app.recipe-mgmt', function ($provide) {
+        $provide.value('currentContextPath', oasp.mock.currentContextPathReturning(contextPath));
+    }));
+
+    beforeEach(inject(function (_recipeManagementRestService_) {
+        recipeManagementRestService = _recipeManagementRestService_;
+    }));
+
+    it('should call $http.get when recipeManagementRestService.getRecipe is called', inject(function ($http) {
+        //given
+        var id = 'recipeId';
+        spyOn($http, 'get');
+        //when
+        recipeManagementRestService.getRecipe(id);
+        //then
+        expect($http.get).toHaveBeenCalledWith(contextPath + 'services/rest/recipemanagement/v1/recipe/' + id);
+    }));
+
+    //Test saveRecipe ToDo
+
+    it('should call $http.post when recipeManagementRestService.getPaginatedRecipes is called', inject(function ($http) {
+        //given
+        spyOn($http, 'post');
+        //when
+        recipeManagementRestService.getPaginatedRecipes(1, 3);
+        //then
+        expect($http.post).toHaveBeenCalledWith(contextPath + 'services/rest/recipemanagement/v1/recipe/search',
+            {
+                pagination: {
+                    size: 3,
+                    page: 1,
+                    total: true
+                }
+            });
+    }));
+
+});
