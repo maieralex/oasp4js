60ef0b58fd2a39e1db0b0b7fc1b9654ebaaa3330
Marc Schwede <schwedenmut@googlemail.com>
2015-11-04 17:28:16 +0100

    inserted tests for gerichte-liste.rest.service and gerichte-liste.service. currently not sure, how to test the graphical changes done by the sign-in.directive.

--- app/main/js/gerichte-liste.service.js
+++ app/main/js/gerichte-liste.service.js
@@ -1,4 +1,5 @@
-angular.module('app.main').factory('gerichteListe', function (gerichteListeRestService) {
+angular.module('app.main')
+    .factory('gerichteListe', function (gerichteListeRestService) {
     'use strict';
     return {
         loadRecipe: function (recipeId) {
@@ -7,33 +8,35 @@ angular.module('app.main').factory('gerichteListe', function (gerichteListeRestS
             });
         },
         loadGerichteListe : function (listLength) {
-
-            // Fisher–Yates shuffle algorithm
-            /*var shuffleArray = function(array) {
-                var m = array.length, t, i;
-                // While there remain elements to shuffle
-                while (m) {
-                    // Pick a remaining element…
-                    i = Math.floor(Math.random() * m--);
-                    // And swap it with the current element.
-                    t = array[m];
-                    array[m] = array[i];
-                    array[i] = t;
-                }
-                return array;
-            };*/
-
-            console.log(listLength);
             var returnArray = [];
             for (var i=0; i < (listLength); i++) {
                 gerichteListeRestService.getRecipe(i).then(function(response) {
                     //dreckiger fallback auf eine fixe URL, sollte imageId null sein.
+
                     if (response.data.imageId == null) response.data.imageId = "http://files.schwedenmut.de/fallback.jpg"
                     returnArray.push(response.data);
-                    //console.log(response.data);
+                    console.log(response.data);
                 });
+                /*gerichteListeRestService.getRecipePicture(i).then(function(response) {
+                    return response;
+                    //console.log(response);
+                })*/
             }
             return returnArray;
         }
     }
-});
+})/*
+    .factory('sessionInjector', function($q, oaspUnauthenticatedRequestResender) {
+        return {
+            responseError: function (response) {
+                var originalRequest;
+                console.log(response.status);
+                if (response.status === 403) {
+                    console.log(response.config);
+                    originalRequest = response.config;
+                    return oaspUnauthenticatedRequestResender.addRequest(originalRequest);
+                } else
+                return $q.reject(response);
+            }
+        };
+    });*/
