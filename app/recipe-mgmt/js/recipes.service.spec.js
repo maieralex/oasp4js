2566c22aa4ac1808498db4618088b6224f08e2cc
Sascha Felbecker <sascha.felbecker@smail.fh-koeln.de>
2015-11-24 13:09:12 +0100

    added mocks and tests for recipe-mgmt services

--- app/recipe-mgmt/js/recipes.service.spec.js
+++ app/recipe-mgmt/js/recipes.service.spec.js
@@ -8,7 +8,28 @@ describe('Service: recipes', function () {
                 total: true
             }
         },
+        recipe = {
+            "id":10000000,
+            "modificationCounter":0,
+            "revision":null,
+            "name":"Test",
+            "description":"Test",
+            "language":null,
+            "price":"15.40",
+            "imageId":null,
+            "author":null,
+            "categories":"Test",
+            "portions":5,
+            "ingredients":"Test",
+            "difficulty":"medium",
+            "prepTimeMinutes":15,
+            "cookTimeMinutes":15,
+            "calories":51000,
+            "cookingInstructions":"Test"
+        },
         recipes,
+        receivedrecipe,
+        savedrecipe,
         $httpBackend,
         listOfRecipes,
         contextPath = '/oasp-app/',
@@ -51,7 +72,59 @@ describe('Service: recipes', function () {
                     ]
                 }
             );
-        };
+        },
+
+
+    mockgetRecipePicture = function () {
+        $httpBackend.whenGET(contextPath + 'services/rest/recipemanagement/v1/recipe/0/picture').respond(
+            'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAIAAAACDbGyAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAQSURBVBhXY/iPCijj//8PAK09SrZrfO6mAAAAAElFTkSuQmCC'
+        );
+    },
+
+    mockSaveRecipe = function () {
+        $httpBackend.whenPOST(contextPath + 'services/rest/recipemanagement/v1/recipe/', recipe).respond(
+            {
+                "id":10000000,
+                "modificationCounter":0,
+                "revision":null,
+                "name":"Test",
+                "description":"Test",
+                "language":null,
+                "price":"15.40",
+                "imageId":null,
+                "author":null,
+                "categories":"Test",
+                "portions":5,
+                "ingredients":"Test",
+                "difficulty":"medium",
+                "prepTimeMinutes":15,
+                "cookTimeMinutes":15,
+                "calories":51000,
+                "cookingInstructions":"Test"
+            }
+        );
+    },
+
+/*    //TODO
+    mocksaveRecipePicture = function () {
+        $httpBackend.whenPOST(contextPath + '/recipe/0/picture',,{headers:{'Content-Type': 'multipart/mixed'}}).respond(
+
+        )
+    },*/
+
+    mockgetRecipe = function () {
+        $httpBackend.whenGET(contextPath + 'services/rest/recipemanagement/v1/recipe/0').respond(
+            {
+                id: 0,
+                modificationCounter: 0,
+                description: 'Schnitzel Wiener Art',
+                imageId : null,
+                name: 'Wienerschnitzel',
+                price: 7.99,
+                revision: null
+            }
+        );
+    };
 
     beforeEach(module('app.recipe-mgmt'));
 
@@ -68,8 +141,16 @@ describe('Service: recipes', function () {
     beforeEach(function () {
         // given // when
         mockPaginatedRecipeResponse();
+        mockSaveRecipe();
+        mockgetRecipe();
         recipes.getPaginatedRecipes(1, 3).then(function (paginatedRecipes) {
             listOfRecipes = paginatedRecipes.result;
+        });
+        recipes.getRecipe(0).then(function(result){
+            receivedrecipe = result.result;
+        });
+        recipes.saveRecipe(recipe).then(function(result){
+            savedrecipe = result.result;
         });
         $httpBackend.flush();
 
@@ -84,6 +165,11 @@ describe('Service: recipes', function () {
         expect(listOfRecipes.length).toBe(3);
         expect(listOfRecipes[0].id).toEqual(0);
 
+    });
+
+    it('uses getRecipe service to receive a recipe from Server', function () {
+        expect(receivedrecipe.id).toBe(0);
+        expect(receivedrecipe.name).toBe('Wienerschnitzel');
     });
 
 });
