0f3f006b35a1b8588c3e096343d8393ccae1834c
Yasa Yener <yyener@uni-koeln.de>
2015-12-02 13:11:06 +0100

    Ingredients can now be added to the table. Ingredients can now be sorted with arrow up/down keys.

--- app/recipe-mgmt/js/recipe-add.controller.js
+++ app/recipe-mgmt/js/recipe-add.controller.js
@@ -37,7 +37,6 @@ angular.module('app.recipe-mgmt')
                 unit: 'g',
                 amount: 300
             });
-            console.log($scope.recipe);
         };
 
         $scope.imageBusy = false;
@@ -52,10 +51,61 @@ angular.module('app.recipe-mgmt')
             $scope.imageDirty = true;
         }, false);
 
+        $scope.addIngredient = function() {
+            if(!$scope.emptyIngredientExists()) {
+                $scope.recipe.ingredients.push({
+                    name: '',
+                    unit: '',
+                    amount: '',
+                });
+            }
+        };
+
+        $scope.emptyIngredientExists = function() {
+            var emptyIngredient = false;
+            for(var i = 0; i < $scope.recipe.ingredients.length; i++) {
+                var ing = $scope.recipe.ingredients[i];
+                if((ing.name === null || ing.name === '') ||
+                    (ing.unit === null || ing.unit === '') ||
+                    (ing.amount === null || ing.amount === '' || ing.amount === 0)) {
+                    emptyIngredient = true;
+                }
+            }
+            return emptyIngredient;
+        }
+
+        $scope.ingredientControls = function(e, element) {
+            var isFirstIngredient = element.ingredient === $scope.recipe.ingredients[0];
+            var isLastIngredient = element.ingredient === $scope.recipe.ingredients[$scope.recipe.ingredients.length-1]
+            // keyCode 9 = TAB
+            if(e.keyCode === 9 && isLastIngredient && !$scope.emptyIngredientExists()) {
+                $scope.recipe.ingredients.push({
+                    name: '',
+                    unit: '',
+                    amount: '',
+                });
+            }
+            if(e.keyCode === 38 && !isFirstIngredient) {
+                var index = $scope.recipe.ingredients.indexOf(element.ingredient);
+                var tmp = $scope.recipe.ingredients[index];
+                $scope.recipe.ingredients[index] = $scope.recipe.ingredients[index-1];
+                $scope.recipe.ingredients[index-1] = tmp;
+                document.getElementById('ingredientTable').getElementsByTagName('tr')[index-1].getElementsByTagName('input')[0].focus();
+            }
+            if(e.keyCode === 40 && !isLastIngredient) {
+                var index = $scope.recipe.ingredients.indexOf(element.ingredient);
+                var tmp = $scope.recipe.ingredients[index];
+                $scope.recipe.ingredients[index] = $scope.recipe.ingredients[index+1];
+                $scope.recipe.ingredients[index+1] = tmp;
+                document.getElementById('ingredientTable').getElementsByTagName('tr')[index+1].getElementsByTagName('input')[0].focus();
+            }
+        };
+
         $scope.saveRecipe = function() {
             if(!$scope.imageDirty) {
                 $scope.recipe.image = null;
             }
+            $scope.recipe.ingredients = null; // Remove this, once ingredients can be saved!!!
         	recipes.saveRecipe($scope.recipe).then(function() {
                 $rootScope.reloadRecipes();
                 $scope.image = null;
